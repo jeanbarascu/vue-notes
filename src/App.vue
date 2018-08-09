ce08f15f2a7eb4b8b67fec6e2c0d552dff877f18
Jean Barascu <narcis86@gmail.com>
2018-08-09 14:46:34 +0300

    HTML structure and Vuex install

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <sidebar></sidebar>
+    <pad></pad>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pad from "./components/Pad";
+import Sidebar from "./components/Sidebar";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Pad,
+    Sidebar
   }
-}
+};
 </script>
 
 <style>
+body,
+html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+body {
+  color: #2c3e50;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  height: 100%;
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="sidebar">
+      <note></note>
+      <note></note>
+      <note></note>
+      <!-- <div class="sidebar__content">
+        Boo, no notes. Get writing!
+      </div> -->
+    </div>
+</template>
+
+<script>
+import Note from "./Note";
+
+export default {
+  components: {
+    Note
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+  background-color: #f9f9f9;
+  flex: 1;
+  height: 100%;
+  max-width: 28%;
+  border-right: 2px solid #eee;
+  overflow-y: scroll;
+  &__content {
+    padding: 30px;
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+  }
+}
+</style>
+
